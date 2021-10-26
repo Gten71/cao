edb90581e8a047725ad82c0da5c27b30b8410709
Maxim <Maxim@DESKTOP-ICMSR5Q>
2021-10-26 20:20:17 +0200

    Add project files.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Запишите значение a");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Запишите значение b");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("Запишите значение c");
+            double c = double.Parse(Console.ReadLine());
+            if (a < 45 && b >= 45 && c >= 45)
+            {
+                Console.Write("Условие является истинным");
+            }
+            else if (a >= 45 && b < 45 && c >= 45)
+            {
+                Console.Write("Условие является истинным");
+            }
+            else if (a >= 45 && b >= 45 && c < 45)
+            {
+                Console.Write("Условие является истинным");
+            }
+            else
+            {
+                Console.Write("Условие не является истинным");
+            }
+        }
+    }
+}
